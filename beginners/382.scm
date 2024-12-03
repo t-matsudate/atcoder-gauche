6637fa5754f3133034ab44b54f1b520d67771c89
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-12-03 07:16:55 +0000

    Added: ABC382B

--- beginners/382.scm
+++ beginners/382.scm
@@ -12,3 +12,18 @@
   (let* ((preconditions (map string->number (string-split (read-line) #\space)))
          (cookies (read-line)))
     (print (count-empties cookies (list-ref preconditions 1)))))
+
+(define (replace-empties cookies eaten)
+  (define cookies-v (string->vector cookies))
+  (define (go eaten pos)
+    (cond ((<= eaten 0) (vector->string cookies-v))
+          ((char=? #\@ (vector-ref cookies-v (- pos 1)))
+           (begin (vector-set! cookies-v (- pos 1) #\.)
+                  (go (- eaten 1) (- pos 1))))
+          (else (go eaten (- pos 1)))))
+  (go eaten (vector-length cookies-v)))
+
+(define (abc382b)
+  (let* ((preconditions (map string->number (string-split (read-line) #\space)))
+         (cookies (read-line)))
+    (print (replace-empties cookies (list-ref preconditions 1)))))
--- tests/beginners/382.scm
+++ tests/beginners/382.scm
@@ -6,3 +6,9 @@
 (test* "(@@@, 3)" 3 (count-empties "@@@" 3))
 (test* "(@@@.@@.@@., 4)" 7 (count-empties "@@@.@@.@@." 4))
 (test-end)
+
+(test-start "ABC382B")
+(test* "(.@@.@, 2)" ".@..." (replace-empties ".@@.@" 2))
+(test* "(@@@, 3)" "..." (replace-empties "@@@" 3))
+(test* "(@@@.@@.@@., 4)" "@@@......." (replace-empties "@@@.@@.@@." 4))
+(test-end)
